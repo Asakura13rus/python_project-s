ca61acf8183ce196bb849c17990bf7d19ed15448
Asakura13rus <asakura13rus@gmail.com>
2024-07-14 11:54:36 +0300

    Initial commit

---
+++ pythonProject/homework2.py
@@ -0,0 +1,6 @@
+home_work = 12
+hour_work = 1.5
+nameOfWork = "Pyhon"
+timeOfWork = hour_work/home_work
+print("Курс:", nameOfWork,"," "Всего задач:",home_work,",","Затрачено часов:",hour_work,",",
+      "среднее время выполнения:", timeOfWork," часа.")
