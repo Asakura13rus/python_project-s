b3d170f49a313d61e9bf4d9426582a2123729359
Asakura13rus <asakura13rus@gmail.com>
2024-07-14 15:25:56 +0300

    Update homework2.py

--- pythonProject/homework2.py
+++ pythonProject/homework2.py
@@ -2,5 +2,5 @@ home_work = 12
 hour_work = 1.5
 nameOfWork = "Pyhon"
 timeOfWork = hour_work/home_work
-print("Курс:", nameOfWork,"," "Всего задач:",home_work,",","Затрачено часов:",hour_work,",",
+print("Курс:", nameOfWork,"\n" "Всего задач:",home_work,"\n", "Затрачено часов:",hour_work,"\n",
       "среднее время выполнения:", timeOfWork," часа.")
