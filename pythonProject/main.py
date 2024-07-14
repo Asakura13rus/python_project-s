4a9c6b66f13acef1a2a296ee945312e3645c13e1
Asakura13rus <asakura13rus@gmail.com>
2024-07-14 18:41:10 +0300

    Add files via upload

---
+++ pythonProject/main.py
@@ -0,0 +1,24 @@
+# 1st program
+
+print("1st:", (9**0.5)*5)
+
+# 2nd program
+
+a = 9.99
+b = 9.98
+c = 1000
+d = 1000.1
+print("2nd:", a > b and c != d or a > b and c != d)
+
+# 3rd program
+first = 1234
+second = 5678
+print("3rd:", first % 1000 // 10 + second % 1000 // 10)
+
+# 4th program
+
+a = "13.42"
+b = "42.13"
+print("4th:", int(a[0:2]) == int(b[3:]) or int(a[3:]) == int(b[0:2]))
+
+
