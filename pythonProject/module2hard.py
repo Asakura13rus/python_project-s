2905e7b4d911fa15a3b9b43b8a1cffb122867b5d
Asakura13rus <asakura13rus@gmail.com>
2024-08-19 13:56:45 +0300

    Update module2hard.py

--- pythonProject/module2hard.py
+++ pythonProject/module2hard.py
@@ -1,12 +1,11 @@
-n = int(input("Введите число от 3 до 20: "))
+n = int(input("Ведите число от 3 до 20 что бы получить пароль: "))
 
 pairs = []
-
-for i in range(1, 21):
-    for j in range(i + 1, 21):
+for i in range(1, n):
+    for j in range(i + 1, n):
         if (i + j) % n == 0:
-            pairs.append(f"{i}{j}")
+            pairs.append(str(i) + str(j))
 
 result = ''.join(pairs)
 
-print(f"{n} - {result}")
+print(f'{n} - {result}')
