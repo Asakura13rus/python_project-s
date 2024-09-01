591c22e67dbd05e85d5ef011e077121b9ff330aa
Asakura13rus <asakura13rus@gmail.com>
2024-09-01 16:44:47 +0300

    Update module2hard.py

--- pythonProject/module2hard.py
+++ pythonProject/module2hard.py
@@ -1,12 +1,10 @@
 for n in range(3, 21):
     result = ""
-    used_pairs = set() 
+    used_pairs = set()  # Для хранения использованных пар
 
     for i in range(1, n):
-        for j in range(1, n):
-            if i != j and (i + j) % n == 0:
-                if (j, i) not in used_pairs:
-                    result += f"{i}{j}"
-                    used_pairs.add((i, j))
+        for j in range(i + 1, n):
+            if (i + j) % n == 0:
+                result += f"{i}{j}"
 
     print(f"{n} - {result}")
