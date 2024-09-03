f18ab95d00972212ae1459e396a8ed63425bf214
Asakura13rus <asakura13rus@gmail.com>
2024-09-03 10:02:51 +0300

    Update module_6_2.py

--- pythonProject/module_6_2.py
+++ pythonProject/module_6_2.py
@@ -1,56 +1,45 @@
 class Vehicle:
-    # Атрибут класса с допустимыми цветами
     __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
     def __init__(self, owner, model, color, engine_power):
-        self.owner = owner  # владелец транспорта (может изменяться)
-        self.__model = model  # модель транспорта (не изменяется)
-        self.__color = color  # цвет транспорта (не изменяется напрямую)
-        self.__engine_power = engine_power  # мощность двигателя (не изменяется напрямую)
+        self.owner = owner
+        self.__model = model
+        self.__color = color
+        self.__engine_power = engine_power
 
-    # Метод для получения модели транспорта
     def get_model(self):
         return f"Модель: {self.__model}"
 
-    # Метод для получения мощности двигателя
     def get_horsepower(self):
         return f"Мощность двигателя: {self.__engine_power}"
 
-    # Метод для получения цвета транспорта
     def get_color(self):
         return f"Цвет: {self.__color}"
 
-    # Метод для вывода всей информации о транспорте
     def print_info(self):
         print(self.get_model())
         print(self.get_horsepower())
         print(self.get_color())
         print(f"Владелец: {self.owner}")
 
-    # Метод для изменения цвета транспорта
     def set_color(self, new_color):
-        # Проверяем, доступен ли новый цвет в списке допустимых цветов (не учитывая регистр)
         if new_color.lower() in [color.lower() for color in self.__COLOR_VARIANTS]:
             self.__color = new_color
         else:
             print(f"Нельзя сменить цвет на {new_color}")
 
 class Sedan(Vehicle):
-    __PASSENGERS_LIMIT = 5  # Атрибут класса для ограничения количества пассажиров
+    __PASSENGERS_LIMIT = 5
 
     def __init__(self, owner, model, color, engine_power):
         super().__init__(owner, model, color, engine_power)
 
-# Пример использования
 vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
 
-# Изначальные свойства
 vehicle1.print_info()
 
-# Меняем свойства (в т.ч. вызывая методы)
 vehicle1.set_color('Pink')
 vehicle1.set_color('BLACK')
 vehicle1.owner = 'Vasyok'
 
-# Проверяем что поменялось
 vehicle1.print_info()
