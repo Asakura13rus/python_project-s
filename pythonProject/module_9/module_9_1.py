9ec223663bc0a9b758060d2b0c2fa02d817664bf
Asakura13rus <asakura13rus@gmail.com>
2024-10-23 23:02:50 +0300

    Update module_9_1.py

--- pythonProject/module_9/module_9_1.py
+++ pythonProject/module_9/module_9_1.py
@@ -1,10 +1,9 @@
 def apply_all_func(int_list, *functions):
-    results = {}  # Создаем пустой словарь для результатов
+    results = {} 
     for func in functions:
-        # Применяем каждую функцию и записываем результат в словарь
+        
         results[func.__name__] = func(int_list)
     return results
 
-# Примеры использования
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
